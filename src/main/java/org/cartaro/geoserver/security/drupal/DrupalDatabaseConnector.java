39d82d358a7c911376ea9a98ec49bb6fce0336fa
Augustus Kling <augustus.kling@geops.de>
2013-04-26 15:03:14 +0200

    Detecting if queries fail due to database maintenance and repeat with delay after maintenance completion. Users won't see failed queries in such case but receive responses a wee bit delayed.

--- src/main/java/org/cartaro/geoserver/security/drupal/DrupalDatabaseConnector.java
+++ src/main/java/org/cartaro/geoserver/security/drupal/DrupalDatabaseConnector.java
@@ -19,67 +19,65 @@ import org.geotools.util.logging.Logging;
  */
 public class DrupalDatabaseConnector {
 	static final Logger LOGGER = Logging.getLogger("org.geoserver.security");
-	
+
 	private Connection connection;
-	
+
 	/**
 	 * Value prepended to Drupal users and roles
 	 */
-	private String instancePrefix;
+	private final String instancePrefix;
 
 	private Timer timer;
 
+	private final DrupalSecurityServiceConfig drupalConfig;
+
 	/**
-	 * Binds an instance to a Drupal database and repeatedly retries if connection fails.
+	 * Binds an instance to a Drupal database and repeatedly retries if
+	 * connection fails.
+	 * 
 	 * @param drupalConfig
 	 * @throws ClassNotFoundException
 	 */
-	public DrupalDatabaseConnector(final DrupalSecurityServiceConfig drupalConfig)
+	public DrupalDatabaseConnector(
+			final DrupalSecurityServiceConfig drupalConfig)
 			throws ClassNotFoundException {
+		this.drupalConfig = drupalConfig;
 		instancePrefix = drupalConfig.getDrupalInstancePrefix();
-		
+
 		Class.forName("org.postgresql.Driver");
-		try{
-			this.connection = this.accquireConnection(drupalConfig);
-		} catch (SQLException e){
-			// Try reconnecting of connection failed
-			LOGGER.log(Level.WARNING, "Cannot connect to database of configuration "+drupalConfig.getName()+". Retrying in 5000ms interval.", e);
-			timer = new Timer();
-			timer.scheduleAtFixedRate(new TimerTask() {
-				@Override
-				public void run() {
-					try {
-						DrupalDatabaseConnector.this.connection = DrupalDatabaseConnector.this.accquireConnection(drupalConfig);
-						timer.cancel();
-						timer = null;
-						LOGGER.log(Level.INFO, "Finally connected to database of configuration "+drupalConfig.getName());
-					} catch (SQLException e) {
-						LOGGER.log(Level.WARNING, "Cannot connect to database of configuration "+drupalConfig.getName(), e);
-					}
-				}
-			}, 5000, 5000);
-		}
 	}
-	
+
 	/**
-	 * Closes database connection if any is still open.
-	 * Subsequent simply won't have any effect.
+	 * Closes database connection if any is still open. Subsequent simply won't
+	 * have any effect.
 	 */
-	public void close(){
-		if(timer!=null){
+	public void close() {
+		if (timer != null) {
 			timer.cancel();
 			timer = null;
-			LOGGER.log(Level.WARNING, "Don't try failing connection attempts to database of configuration "+instancePrefix+" any longer.");
+			LOGGER.log(Level.WARNING,
+					"Don't try failing connection attempts to database of configuration "
+							+ instancePrefix + " any longer.");
 		}
-		if(this.connection!=null){
+		if (this.connection != null) {
 			try {
 				this.connection.close();
+				this.connection = null;
 			} catch (SQLException e) {
-				LOGGER.log(Level.WARNING, "Could not close database connection of "+instancePrefix, e);
+				LOGGER.log(Level.WARNING,
+						"Could not close database connection of "
+								+ instancePrefix, e);
 			}
 		}
 	}
 
+	/**
+	 * Tries to get a new database connection.
+	 * 
+	 * @param drupalConfig
+	 * @return New database connection.
+	 * @throws SQLException
+	 */
 	protected Connection accquireConnection(
 			DrupalSecurityServiceConfig drupalConfig) throws SQLException {
 		String connectionURL = "jdbc:postgresql://"
@@ -91,9 +89,21 @@ public class DrupalDatabaseConnector {
 		return DriverManager.getConnection(connectionURL);
 	}
 
-	public ResultSet getResultSet(String query) throws SQLException {
-		Statement statement = this.connection.createStatement();
-		return statement.executeQuery(query);
+	/**
+	 * @param query
+	 *            SQL query to run.
+	 * @return Query result
+	 * @throws SQLException
+	 */
+	public ResultSet getResultSet(final String query) throws SQLException {
+		return new SqlTask() {
+			@Override
+			protected ResultSet perform(Connection connection)
+					throws SQLException {
+				Statement statement = connection.createStatement();
+				return statement.executeQuery(query);
+			}
+		}.run();
 	}
 
 	public GeoServerRole stripInstancePrefix(GeoServerRole role) {
@@ -102,11 +112,25 @@ public class DrupalDatabaseConnector {
 		return new GeoServerRole(unprefixed);
 	}
 
-	public ResultSet getResultSet(String query, String parameter)
+	/**
+	 * @param query
+	 *            SQL query to run.
+	 * @param parameter
+	 *            Value for the placeholder.
+	 * @return Query result.
+	 * @throws SQLException
+	 */
+	public ResultSet getResultSet(final String query, final String parameter)
 			throws SQLException {
-		PreparedStatement ps = this.connection.prepareStatement(query);
-		ps.setString(1, parameter);
-		return ps.executeQuery();
+		return new SqlTask() {
+			@Override
+			protected ResultSet perform(Connection connection)
+					throws SQLException {
+				PreparedStatement ps = connection.prepareStatement(query);
+				ps.setString(1, parameter);
+				return ps.executeQuery();
+			}
+		}.run();
 	}
 
 	public GeoServerRole addInstancePrefix(GeoServerRole role) {
@@ -116,41 +140,213 @@ public class DrupalDatabaseConnector {
 	/**
 	 * @param prefixed
 	 * @return
-	 * @throws IllegalArgumentException When prefix can't be stripped because it is not there
+	 * @throws IllegalArgumentException
+	 *             When prefix can't be stripped because it is not there
 	 */
 	public String stripInstancePrefix(String prefixed) {
-		if(!hasInstancePrefix(prefixed)){
-			throw new IllegalArgumentException("Does not have prefix to be stripped: "+prefixed);
+		if (!hasInstancePrefix(prefixed)) {
+			throw new IllegalArgumentException(
+					"Does not have prefix to be stripped: " + prefixed);
 		}
 		return prefixed.substring(instancePrefix.length());
 	}
-	
+
 	/**
-	 * @param prefixed Text to check for sharing prefix with this instance's connection
+	 * @param prefixed
+	 *            Text to check for sharing prefix with this instance's
+	 *            connection
 	 * @return True if prefixes are shared
 	 */
-	public boolean hasInstancePrefix(String prefixed){
+	public boolean hasInstancePrefix(String prefixed) {
 		return prefixed.startsWith(instancePrefix);
 	}
 
 	/**
 	 * Prepend a value with and instance prefix
+	 * 
 	 * @param string
 	 * @return
 	 */
 	public String addInstancePrefix(String string) {
 		return instancePrefix + string;
 	}
-	
+
 	/**
-	 * @return TRUE whilst the bound Drupal instance is still installing its core (initial installation)
+	 * @return TRUE whilst the bound Drupal instance is still installing its
+	 *         core (initial installation)
 	 * @throws SQLException
 	 */
-	public boolean isDrupalCurrentlyInstalling() throws SQLException{
-		// Convert using convert_from to get consistent behavior in Postgres 8 and 9
-		ResultSet drupalCurrentlyInstallingSet = this.getResultSet("select convert_from(value, 'UTF-8')='s:23:\"install_profile_modules\";' as install_profile_modules " +
-				"from variable where name='install_task'");
-		boolean drupalCurrentlyInstalling = drupalCurrentlyInstallingSet.next() && drupalCurrentlyInstallingSet.getBoolean("install_profile_modules");
+	public boolean isDrupalCurrentlyInstalling() throws SQLException {
+		// Convert using convert_from to get consistent behavior in Postgres 8
+		// and 9.
+		ResultSet drupalCurrentlyInstallingSet = this
+				.getResultSet("select convert_from(value, 'UTF-8')='s:23:\"install_profile_modules\";' as install_profile_modules "
+						+ "from variable where name='install_task'");
+		boolean drupalCurrentlyInstalling = drupalCurrentlyInstallingSet.next()
+				&& drupalCurrentlyInstallingSet
+						.getBoolean("install_profile_modules");
 		return drupalCurrentlyInstalling;
+	}
+
+	/**
+	 * Acts on the database connection and repeat actions if failed due to
+	 * missing database. Can only be run once.
+	 */
+	abstract class SqlTask {
+		/**
+		 * Maximum number of times {@link #perform(Connection)} is tried before
+		 * giving up.
+		 */
+		protected int maxRetries = 3;
+
+		/**
+		 * Milliseconds to wait before trying {@link #perform(Connection)}
+		 * again.
+		 */
+		protected int retryInterval = 10000;
+
+		/**
+		 * Query result determined during retries.
+		 */
+		private ResultSet result;
+
+		private boolean used = false;
+
+		/**
+		 * Job that is to be done using the given connection. Called multiple
+		 * times in case of connection failures.
+		 * 
+		 * @param connection
+		 * @return Result to return when {@link #run()} gets called.
+		 * @throws SQLException
+		 */
+		protected abstract ResultSet perform(Connection connection)
+				throws SQLException;
+
+		/**
+		 * @param e
+		 * @return True if exception denotes a connection failure / allows for
+		 *         retrying.
+		 */
+		boolean isRetyable(SQLException e) {
+			// See codes in
+			// http://www.postgresql.org/docs/current/static/errcodes-appendix.html
+			String[] retryables = { "08", "57" };
+			for (String sqlStateStart : retryables) {
+				if (e.getSQLState().startsWith(sqlStateStart)) {
+					return true;
+				}
+			}
+			return false;
+		}
+
+		/**
+		 * Ensure a database connection is available and calls
+		 * {@link #perform(Connection)}.
+		 * 
+		 * @return Whatever {@link #perform(Connection)} returns.
+		 * @throws SQLException
+		 */
+		private ResultSet accquireAndPerform() throws SQLException {
+			if (connection == null) {
+				connection = DrupalDatabaseConnector.this
+						.accquireConnection(drupalConfig);
+			}
+			return this.perform(connection);
+		}
+
+		/**
+		 * Calls perform repeatedly until it succeeds or {@link #maxRetries}
+		 * reached.
+		 * 
+		 * @return Result of {@link #perform(Connection)}.
+		 * @throws SQLException
+		 */
+		public ResultSet run() throws SQLException {
+			if (used) {
+				throw new IllegalStateException(
+						"Task has already been run. Aborted run as internal state might be tainted.");
+			}
+			used = true;
+
+			try {
+				// Try execution without delays.
+				return accquireAndPerform();
+			} catch (SQLException e) {
+				if (isRetyable(e)) {
+					// Discards invalid database connection.
+					DrupalDatabaseConnector.this.close();
+
+					final Timer timer = new Timer();
+					timer.scheduleAtFixedRate(new TimerTask() {
+
+						/**
+						 * Retries calling {@link #perform(Connection)}.
+						 */
+						@Override
+						public void run() {
+							maxRetries = maxRetries - 1;
+							synchronized (SqlTask.this) {
+								if (maxRetries > 0) {
+									try {
+										result = accquireAndPerform();
+										abortRetrying(timer);
+									} catch (SQLException e) {
+										if (isRetyable(e)) {
+											DrupalDatabaseConnector.this
+													.close();
+
+											// Await next interval.
+											LOGGER.log(
+													Level.INFO,
+													"Failed query, will repeat.",
+													e);
+										} else {
+											abortRetrying(timer);
+										}
+									}
+								} else {
+									abortRetrying(timer);
+								}
+							}
+						}
+
+						/**
+						 * Cancels retrying and propagates result.
+						 * 
+						 * @param timer
+						 */
+						private void abortRetrying(final Timer timer) {
+							timer.cancel();
+							SqlTask.this.notify();
+						}
+
+					}, retryInterval, retryInterval);
+
+					// Await availability of result.
+					try {
+						synchronized (this) {
+							// Keep waiting until query result is known.
+							while (maxRetries > 0 && result == null) {
+								this.wait();
+							}
+						}
+						LOGGER.info("done waiting");
+					} catch (InterruptedException e1) {
+						LOGGER.info("interrupt in db recovery");
+					}
+					if (result == null) {
+						// Report original failure.
+						LOGGER.severe("got result after recovery");
+						throw e;
+					} else {
+						// Report result determined by retries.
+						LOGGER.info("got result after recovery");
+						return result;
+					}
+				}
+				throw e;
+			}
+		}
 	}
 }
